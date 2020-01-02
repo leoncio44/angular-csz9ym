76737b9b4e96fb3ad4d583c568adad3e12ba3a0e
leoncio44@gmail.com <leoncio44@gmail.com>
2020-01-03 00:36:01 +0100

    limpieza comentarios

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { Products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -8,7 +8,7 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+  products = Products;
 
   share() {
     window.alert('The product has been shared!');
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,4 +1,4 @@
-export const products = [
+export const Products = [
   {
     name: 'Phone XL',
     price: 799,
@@ -15,10 +15,3 @@ export const products = [
     description: ''
   }
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
